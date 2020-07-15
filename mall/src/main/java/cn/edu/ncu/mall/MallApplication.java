2a1efaa1e177f8c1a08662c649623a0afc14baca
lyw <2354640364@qq.com>
2020-07-15 09:37:39 +0800

    :tada:init

---
+++ mall/src/main/java/cn/edu/ncu/mall/MallApplication.java
@@ -0,0 +1,19 @@
+package cn.edu.ncu.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Application.
+ * @author lyw
+ * @author 2354640364@qq.com
+ */
+
+@SpringBootApplication
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}
